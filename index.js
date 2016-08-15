c74379bd6331cbb8bbc02038fb2ff22f2308d96e
tehmou <timo.tuominen@gmail.com>
2016-08-15 13:08:30 +0100

    Initial commit.

---
+++ index.js
@@ -0,0 +1,21 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
+
+app.get('/', function(req, res){
+  res.sendFile(INDEX);
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(PORT, function(){
+  console.log('listening on *:' + PORT);
+});
