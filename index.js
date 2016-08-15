0631b6f0e1d1bb606cb83855ba6a0d3d7804fbdf
tehmou <timo.tuominen@gmail.com>
2016-08-15 13:10:31 +0100

    Add log to incoming messages

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
+    console.log('chat message: ' + msg);
     io.emit('chat message', msg);
   });
 });
